242b2d15d7840266bb5729399a2483505146a961
Radilovk <radilov.k@gmail.com>
2025-08-18 05:18:48 +0300

    feat: добави Access-Control-Max-Age и тест

--- worker.js
+++ worker.js
@@ -655,6 +655,7 @@ function corsHeaders(request, env = {}, additionalHeaders = {}) {
         "Access-Control-Allow-Origin": origin,
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        "Access-Control-Max-Age": "86400",
         ...additionalHeaders,
     };
 
